5bd1165ace69214b7bb86b8c08e969590a6632f8
Jounnyy <agrieva.xananda08@gmail.com>
2024-05-21 16:14:26 +0700

    [checking] ~ camera page

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,39 +1,63 @@
 import { Img } from "@/components/image";
 import cam1 from "@/assets/cam1.png";
 import React from "react";
-import {RadioGroup, Radio} from "@nextui-org/react";
+import { RadioGroup, Radio } from "@nextui-org/react";
+import { ArrowRight, MapPin, Time } from "@/components/icons";
 
 export default function Home() {
-	return (
-		<section className="flex flex-col items-center justify-center gap-4 md:py-10">
-			<div className="w-10/12">
-				<Img 
-					src={cam1}
-					alt="cam1"
-					className="m-0 p-0 w-max h-auto"
-				/>
-			</div>
-			<div className="flex flex-row w-full gap-3 justify-between">
-				<div className="w-full h-auto p-4">
-					<h5 className="font-semibold text-lg">Pilih Kamera</h5>
-					<form action="" method="get">
-					<RadioGroup
-						orientation="horizontal"
-						defaultValue="cam1"
-						className="mt-3"
-						>
-						<Radio value="cam1" className="mr-1">Kamera 1</Radio>
-						<Radio value="cam2" className="mr-1">Kamera 2</Radio>
-						</RadioGroup>
-					</form>
-				</div>
-				<div className="w-full h-auto p-4">
-					<i className="ri-map-pin-fill"></i>
-				</div>
-				<div className="w-full h-auto p-4">
-					tes
-				</div>
-			</div>
-		</section>
-	);
+  return (
+    <section className="flex flex-col items-center justify-between gap-3 md:py-2">
+      <div className="flex-grow w-full flex justify-center items-center p-1">
+        <Img
+          src={cam1}
+          alt="cam1"
+          className="m-0 p-0 w-9/12 h-auto"
+        />
+      </div>
+      <div className="flex flex-row w-full gap-3 justify-between px-4">
+        <div className="w-full h-auto p-4">
+          <h5 className="font-semibold text-lg">Pilih Kamera</h5>
+          <form action="" method="get">
+            <RadioGroup
+              orientation="horizontal"
+              defaultValue="cam1"
+              className="mt-3"
+            >
+              <Radio value="cam1" className="mr-1">Kamera 1</Radio>
+              <Radio value="cam2" className="mr-1">Kamera 2</Radio>
+            </RadioGroup>
+          </form>
+        </div>
+        <div className="flex flex-col w-full p-4 items-center">
+          <div className="flex flex-row w-full mt-2">
+            <div className="mr-2">
+              <MapPin color="#3b82f6" />
+            </div>
+            <p className="font-semibold">
+              Jalan Raya Margonda Raya, Depok
+            </p>
+          </div>
+          <div className="flex flex-row w-full mt-2">
+            <div className="mr-2">
+              <Time color="#3b82f6" />
+            </div>
+            <p className="font-semibold">
+              12.45 WIB
+            </p>
+          </div>
+        </div>
+        <div className="flex flex-row w-full mt-2">
+          <div className="relative w-11/12 p-4 bg-blue-500 rounded-md text-white flex items-center justify-between">
+            <div className="flex flex-col space-y-1"> {/* Added space-y-1 */}
+              <span className="font-semibold text-base">Kamis, 9 Mei 2024</span> {/* Changed text-base */}
+              <span className="text-sm">Pelanggaran hari ini: 7</span>
+            </div>
+            <div className="h-10 w-10 rounded-full flex items-center justify-center"> {/* Placeholder for icon */}
+              <ArrowRight color="#FAFAFA" width={50} height={50}/>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
